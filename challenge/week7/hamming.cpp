cfa6c0623c612828d6d323a0ae5fba78f829581b
sodifficult333 <seunghan7580@gmail.com>
2024-10-14 16:25:38 +0900

    Function and String Exercises

--- challenge/week7/hamming.cpp
+++ challenge/week7/hamming.cpp
@@ -2,31 +2,33 @@
 #include <string>
 using namespace std;
 
-string toLowerStr(string str) {
-    for (int i = 0; i<str.length(); i++)
-        tolower(str[0]);
-    return str;
+string toLowerStr(string str) { //소문자로 바꿔주는 함수
+    for (char &c : str) {
+        c = tolower(c);
+    }
+    return str; //바꾼 문자열을 반환
 }
 
-string toUpperStr(string str) {
-    for (int i = 0; i<str.length(); i++)
-    toupper(str[0]);
-    return str;
+string toUpperStr(string str) { //대문자로 바꿔주는 함수
+    for (char &c : str) {
+        c = toupper(c);
+    }
+    return str; //바꾼 문자열을 반환
 }
 
-int calcHammingDist(string s1, string s2) {
+int calcHammingDist(string s1, string s2) { //해밍거리를 계산하는 함수
     int count = 0;
-    if(s1.length() != s2.length()) {
+    if(s1.length() != s2.length()) { //길이가 다를 때 오류를 출력
         cout << "오류 : 길이가 다름" << endl;
         return 0;
     }
     else {
-        for(int i = 0; i < s1.length(); i++) {
+        for(int i = 0; i < s1.length(); i++) { //길이가 같을 때 해밍거리를 계산
             if(s1[i] != s2[i])
             count += 1;
         }
     }
-    return count;
+    return count; //count를 반환
 }
 
 int main() {
@@ -37,9 +39,11 @@ int main() {
     cout << "DNA2: ";
     cin >> s2;
 
-    toLowerStr(s1);
-    toUpperStr(s2);
+    string a; //소문자로 바꾼 문자열을 저장할 스트링 변수
+    string b; 
+    a = toLowerStr(s1);
+    b = toLowerStr(s2);
 
-    cout << "해밍 거리는 " << calcHammingDist(s1, s2) << endl;
+    cout << "해밍 거리는 " << calcHammingDist(a, b) << endl; //소문자로 바꾼 문자열을 비교해서 해밍거리 계산
     return 0;
 }
